2692f5156538de70b82da132ab5403cdb9e5073c
Alexander Yammine <alexyammine94@gmail.com>
2019-01-25 11:59:11 -0400

    New changes

--- api/controllers/medical-file/post-medical-file.js
+++ api/controllers/medical-file/post-medical-file.js
@@ -1,4 +1,7 @@
 var ipfs = require("../../../ipfs-api/ipfs_api");
+var fabric = require("../../../fabric-api/chaincodeTransactions");
+var key = require('../../../crypto/keys');
+const fs = require('fs');
 
 module.exports = {
 
@@ -35,6 +38,10 @@ module.exports = {
     ipfs: {
       responseType: 'ipfs-error2',
       description: 'Error subiendo la imagen'
+    },
+    ursa: {
+      responseType: 'ursa-error',
+      description: 'Error en la clave enviada'
     }
   },
 
@@ -42,31 +49,42 @@ module.exports = {
     // If one of required parameters is missing
     if(!inputs.imageFile || !inputs.imageName) 
       return exits.invalid();
-
+    
     // Generate the new name of file
     let imageFileName = await sails.helpers.strings.random('url-friendly') + '.' + inputs.imageName.split('.').pop();
 
     // Save the Image
     inputs.imageFile.upload({ dirname: require('path').resolve(sails.config.appPath, 'assets/images/medical/'), saveAs: imageFileName }, function (err, uploadedFile){
       if (err) return res.serverError(err);
+      // Image path
+      var path = 'assets/images/medical/'+imageFileName;
+      /* -------------------------------------- */
+      // Encrypt file
+      if ( inputs.encrypt ) {
+        try{
+          // Read public key
+          // var pubKey = ursa.createPublicKey(inputs.secretKey,'base64');
+        }catch(err){
+          return exits.ursa();
+        }
+      /* -------------------------------------- */
+      }else{
+        // No encrypt file
+        //IPFS
+        ipfs.upload(path,(err, hashFile) => {
+          if (err)
+            return exits.ipfs();
 
-      //IPFS
-      ipfs.upload('assets/images/medical/'+imageFileName,(err, hashFile) => {
-        if (err)
-          return exits.ipfs();
-
-        return exits.success(
-          { 
-            success: true, 
-            message: inputs.imageName + ' uploaded successfully to IPFS network. A search hash for this file has been generated.', 
-            ipfsMessage: 'The file is now publicly available from: ',
-            ipfsUrl: 'https://gateway.ipfs.io/ipfs/' + hashFile,
-            hash: hashFile
-          });
-      });
+          return exits.success(
+            { 
+              success: true, 
+              message: inputs.imageName + ' uploaded successfully to IPFS network. A search hash for this file has been generated.', 
+              ipfsMessage: 'The file is now publicly available from: ',
+              ipfsUrl: 'https://gateway.ipfs.io/ipfs/' + hashFile,
+              hash: hashFile
+            });
+        });
+      }
     });
-
   }
-
-
 };
