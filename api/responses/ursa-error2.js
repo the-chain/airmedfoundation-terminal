21a28accecb82d5318e46ea979f1bf27af800cfe
Alexander Yammine <alexyammine94@gmail.com>
2019-01-26 16:43:18 -0400

    New respones - Private key error

---
+++ api/responses/ursa-error2.js
@@ -0,0 +1,17 @@
+/**
+ * ursa-error.js
+**/
+
+module.exports = function ursaError() {
+
+    var res, result;
+    
+    res = this.res;
+
+    result = {
+        status: 400,
+        message: 'Error. The provided private key is not valid'
+    };
+  
+    return res.status(400).json(result);
+};
