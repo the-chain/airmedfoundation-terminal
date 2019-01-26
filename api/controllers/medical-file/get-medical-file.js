8e063ca89e46910fa572c2cf92caf992f1e49217
Alexander Yammine <alexyammine94@gmail.com>
2019-01-26 16:44:12 -0400

    Added download and decrypt file

--- api/controllers/medical-file/get-medical-file.js
+++ api/controllers/medical-file/get-medical-file.js
@@ -2,6 +2,7 @@ var ipfs = require("../../../ipfs-api/ipfs_api");
 const fs = require('fs');
 const sleep = require('sleep');
 const fileType = require('file-type');
+const ursa = require('../../../crypto/keys');
 
 module.exports = {
 
@@ -37,6 +38,10 @@ module.exports = {
     ipfs: {
       responseType: 'ipfs-error',
       description: 'Imagen no encontrada o hash inválido'
+    },
+    ursa:{
+      responseType: 'ursa-error2',
+      description: 'Error en la clave privada'
     }
   },
 
@@ -46,9 +51,20 @@ module.exports = {
     // If one of required parameters is missing
     if(!inputs.ipfsHash)
       return exits.invalid();
-    
+    var hash;
+    // Encrypted
+    if (inputs.encrypted) {
+      try{
+        await ursa.assertPrivateKey(inputs.secretKey);
+      }catch(err){
+        return exits.ursa();
+      }
+      hash = await ursa.decryptIpfsHash(inputs.secretKey, inputs.ipfsHash);
+    }else{
+      hash = inputs.ipfsHash;
+    }
     // Get image from ipfs
-    ipfs.download(inputs.ipfsHash, (err,file) => {
+    ipfs.download(hash, (err,file) => {
       if (err) 
         return exits.ipfs();
 
@@ -60,20 +76,20 @@ module.exports = {
           mime: 'file/unknownType'
         }
       }
-      var path = 'assets/images/' + inputs.ipfsHash + '.' + type.ext;
+      var path = 'assets/images/' + hash + '.' + type.ext;
       fs.writeFile(path, file, 'binary', (err)=>{
         if (err) 
           return exits.write();
         
-        sleep.sleep(3);
+        sleep.sleep(2);
         var datetime = new Date();
         return exits.success(
           { 
             success: true, 
             message: 'IPFS hash match with the following image',
             ipfsMessage: 'The file is publicly available from: ',
-            ipfsUrl: 'https://gateway.ipfs.io/ipfs/' + inputs.ipfsHash,
-            image: 'images/' + inputs.ipfsHash + '.' + type.ext, 
+            ipfsUrl: 'https://gateway.ipfs.io/ipfs/' + hash,
+            image: 'images/' + hash + '.' + type.ext, 
             imageName: datetime + '.' + type.ext,
             imageType: type.mime
           });
