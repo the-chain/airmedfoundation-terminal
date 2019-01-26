116823ca303e872cd6ea3fd1880d498de4ed3262
Alexander Yammine <alexyammine94@gmail.com>
2019-01-26 13:24:13 -0400

    Bug fixes

--- api/controllers/files/get-files.js
+++ api/controllers/files/get-files.js
@@ -32,23 +32,31 @@ module.exports = {
     // If one of required parameters is missing
     if(!inputs.publicKey)
       return exits.invalid();
-    var response;
+    var response, resp1, resp2;
     // Check private/public key
-    if ( ursa.isPrivateKey(inputs.publicKey) ) {
-      response = await fabric.queryChaincode('mychannel','Org1MSP','airmed4','query',[inputs.publicKey]);
-    }else if ( ursa.isPublicKey(inputs.publicKey) ) {
+    if ( await ursa.isPrivateKey(inputs.publicKey) ) {
+      var arg = await ursa.getPublicKey(inputs.publicKey);
+      response = await fabric.queryChaincode('mychannel','Org1MSP','airmed4','query',[arg]);
+    }else if ( await ursa.isPublicKey(inputs.publicKey) ) {
       response = await fabric.queryChaincode('mychannel','Org1MSP','airmed4','query',[inputs.publicKey]);
     }else{
       return exits.ursa();
     }
-    response = JSON.parse(response.toString('utf8'));
+    resp1 = response[0].toString('utf8');
+    resp2 = response[1].toString('utf8');
+    if (!resp1)
+      resp1 = '{"hashSent":[],"hashReceived":[]}';
+    if (!resp2)
+      resp2 = '{"hashSent":[],"hashReceived":[]}';
     // Check response from Fabric Peer
-    if ( response[0] ){
-      response = response[0]
-    }else if(response[1]){
-      response = response[1]
-    }else{
-      return exits.fabric();
+    try{
+      response = JSON.parse(resp1);
+    }catch(err){
+      try{
+        response = JSON.parse(resp2);
+      }catch(err){
+        return exits.fabric();
+      }
     }
     // Send response
     return exits.success(
