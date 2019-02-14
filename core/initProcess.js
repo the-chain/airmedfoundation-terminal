018112d20292a8bf5e7a70ff2ba12e749d3b1ec1
Alexander Yammine <alexyammine94@gmail.com>
2019-02-14 10:27:01 -0400

    Error handling

--- core/initProcess.js
+++ core/initProcess.js
@@ -1,10 +1,18 @@
 var sync = require('./sync');
 var listen = require('./listener')
+const slp = require('sleep');
 
 
-async function start() {
-    await sync.startSync('mychannel','Org1MSP',0);
-    //await listen.startListener('mychannel');
+module.exports = {
+    async start() {
+        try {
+        await sync.startSync('mychannel','Org1MSP',0);
+        }catch(err){
+            console.log(err);
+            console.log("Trying again in few second");
+            slp.sleep(5);
+            await this.start();
+        }
+        //await listen.startListener('mychannel');
+    }
 }
-
-start();
