2faff6f29789826151a77e4d8fe33ba0bb72a26a
Alexander Yammine <alexyammine94@gmail.com>
2019-02-14 16:18:51 -0400

    Init process

--- core/initProcess.js
+++ core/initProcess.js
@@ -3,17 +3,17 @@ var listen = require('./listener')
 const slp = require('sleep');
 
 
-module.exports = {
-    async start() {
-        try {
-        await sync.startSync('mychannel','Org1MSP',0);
+async function start() {
+    try {
+    await sync.startSync('mychannel','Org1MSP',0);
+    //await listen.startListener('mychannel');
+    }catch(err){
+        console.log(err);
+        console.log("Trying again in few second");
+        slp.sleep(5);
+        await this.start();
         //await listen.startListener('mychannel');
-        }catch(err){
-            console.log(err);
-            console.log("Trying again in few second");
-            slp.sleep(5);
-            await this.start();
-            //await listen.startListener('mychannel');
-        }
     }
 }
+
+start();
