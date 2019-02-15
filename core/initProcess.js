37d80032ae96850023ac7a780ff5b6d1a0a55ee1
Alexander Yammine <alexyammine94@gmail.com>
2019-02-15 16:22:33 -0400

    Listener completed

--- core/initProcess.js
+++ core/initProcess.js
@@ -4,17 +4,16 @@ const slp = require('sleep');
 
 
 async function start() {
-    //while(true){
+    while(true){
         try {
             await sync.startSync('mychannel','Org1MSP',0);
-            //await listen.startListener('mychannel');
+            await listen.startListener('mychannel', 'Org1MSP',0);
         }catch(err){
             console.log(err);
             console.log("Trying again in few second");
             slp.sleep(10);
-            //await listen.startListener('mychannel');
         }
-    //}
+    }
 }
 
 start();
--- core/listener.js
+++ core/listener.js
@@ -1,18 +1,106 @@
 var Client = require('fabric-client');
 var Channel = require('fabric-client').Channel;
 var client = Client.loadFromConfig("./fabric-api/config/configfile.yaml");
-
+var httpClient = require('./api/httpApi');
+var ledgerQuery = require('../fabric-api/queryLedger');
 
 module.exports = {
-    async startListener(channelName){
+    async startListener(channelName, mspId, peerNumber){
         var channel = new Channel(channelName,client);
-        const eventHub = channel.newChannelEventHub(client.getPeersForOrg('Org1MSP')[0]);
-        block_reg = eventHub.registerBlockEvent((block) => {
-            console.log('Successfully received the block event');
-            console.log(block);
+        const eventHub = channel.newChannelEventHub(client.getPeersForOrg(mspId)[peerNumber]);
+        block_reg = eventHub.registerBlockEvent(async (block) => {
+            try{
+                await this.createBlock(block);
+            }catch(err){
+                console.log(err);
+            }
         }, (error)=> {
             console.log('Failed to receive the block event ::'+error);
         });
         eventHub.connect(true);
+    },
+    async createBlock(block){
+        let blockInfo = {
+            hash: await ledgerQuery.getBlockHash(block.header),
+            number: parseInt(block.header.number),
+            timestamp: Date.parse(block.data.data[0].payload.header.channel_header.timestamp),
+            previous_hash: block.header.previous_hash,
+            data_hash: block.header.data_hash
+        }
+        // Create block
+        await httpClient.createBlock(blockInfo);
+        var N = block.data.data.length;
+        for ( j = 0; j < N; j++ ){
+            let Transaction = {};
+            Transaction.block = blockInfo.hash;
+            Transaction.number = j;
+            Transaction.timestamp = Date.parse(block.data.data[j].payload.header.channel_header.timestamp);
+            Transaction.channel = block.data.data[j].payload.header.channel_header.channel_id;
+            Transaction.id = block.data.data[j].payload.header.channel_header.tx_id;
+            if ( Transaction.id == "")
+                Transaction.id = "0";
+            Transaction.type = block.data.data[j].payload.header.channel_header.typeString;
+            Transaction.creator = block.data.data[j].payload.header.signature_header.creator.Mspid
+            try {
+                Transaction.chaincode = block.data.data[j].payload.data.actions[0].payload.action.proposal_response_payload.extension.chaincode_id;
+            }catch(err){
+                Transaction.chaincode = {
+                    name: "",
+                    version: ""
+                }
+            }
+            try {
+                Transaction.imputsArgs = {
+                    args: []
+                };
+                const totalArgs = block.data.data[j].payload.data.actions[0].payload.chaincode_proposal_payload.input.chaincode_spec.input.args.length;
+                for ( k = 0; k < totalArgs; k++)
+                    Transaction.imputsArgs.args.push(block.data.data[j].payload.data.actions[0].payload.chaincode_proposal_payload.input.chaincode_spec.input.args[k].toString('utf8'));
+            }catch(err){
+                Transaction.imputsArgs.args = [];
+            }
+            try {
+                Transaction.peerEndorsment = {
+                    peers: []
+                };
+                const totalEnd = block.data.data[j].payload.data.actions[0].payload.action.endorsements.length;
+                for ( k = 0; k < totalEnd ; k++ )
+                    Transaction.peerEndorsment.peers.push(block.data.data[j].payload.data.actions[0].payload.action.endorsements[k].endorser.Mspid);
+            }catch(err){
+                Transaction.peerEndorsment.peers = [];
+            }
+            // Create Transaction
+            await httpClient.createTransaction(Transaction);
+            let key = {
+                id: Transaction.id,
+                keys: {}
+            }
+            try{
+                const arg = block.data.data[j].payload.data.actions[0].payload.chaincode_proposal_payload.input.chaincode_spec.input.args[0].toString('utf8');
+                if ( arg != 'upgrade' ){
+                    key.keys = block.data.data[j].payload.data.actions[0].payload.action.proposal_response_payload.extension.results.ns_rwset[1].rwset.reads[0];
+                }
+            }catch(err){
+                key.keys = {};
+            }
+            // Create from chaincode
+            await httpClient.createKeyInChaincode(key);
+            var tempKey;
+            try {
+                tempKey = block.data.data[j].payload.data.actions[0].payload.action.proposal_response_payload.extension.results.ns_rwset[0].rwset.reads;
+            }catch(err){tempKey = null}
+            // Create key in
+            if ( tempKey != null)
+                for ( t = 0; t < tempKey.length; t++ )
+                    await httpClient.createKeyIn({id: Transaction.id, keys: tempKey[t]});
+            
+            try{
+                tempKey = block.data.data[j].payload.data.actions[0].payload.action.proposal_response_payload.extension.results.ns_rwset[0].rwset.writes;
+            }catch(err){tempKey = null}
+            // Create keys out
+            if ( tempKey != null)
+                for ( t = 0; t < tempKey.length; t++ ) 
+                    await httpClient.createKeyOut({id: Transaction.id, keys: tempKey[t]});
+        }
     }
 }
