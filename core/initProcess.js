1bd929103c9571232bed9fa5abeb5e3b761d6b73
alexander <Alexander_yammine@hotmail.com>
2019-02-21 08:50:57 -0400

    Loop removed from sync process

--- core/initProcess.js
+++ core/initProcess.js
@@ -4,7 +4,7 @@ const slp = require('sleep');
 
 
 async function start() {
-    while(true){
+    //while(true){
         try {
             await sync.startSync('mychannel','Org1MSP',0);
             await listen.startListener('mychannel', 'Org1MSP',0);
@@ -13,7 +13,7 @@ async function start() {
             console.log("Trying again in few second");
             slp.sleep(10);
         }
-    }
+    //}
 }
 
 start();
