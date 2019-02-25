26a1527b33a08a7d8eebe3afa587bbd26324571f
alexander <Alexander_yammine@hotmail.com>
2019-02-25 14:11:26 -0400

    New policy to create blocks,transactions and keys

--- api/policies/checkOrigin.js
+++ api/policies/checkOrigin.js
@@ -1,8 +1,6 @@
 module.exports = async function (req, res, proceed) {
-    console.log(req.method);
-    console.log(req.originalUrl);
-    var host = '127.0.0.1';
-    if (req.hostname == host )
+    var host = '::ffff:127.0.0.1';
+    if (req.connection.remoteAddress == host)
         return proceed();
     return res.forbidden();
   };
