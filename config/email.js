a6a96b8cf125b0f7351c507b4276e18575db2a97
alexander <Alexander_yammine@hotmail.com>
2019-03-22 11:06:18 -0400

    EmailVerification status

--- config/email.js
+++ config/email.js
@@ -16,5 +16,7 @@ module.exports.email = {
         user: config.email.auth.user,
         pass: config.email.auth.pass
     },
+    //  0 = No verification, 1 = User verification, 2 = Admin verification
+    emailVerification: config.email.emailVerification,
     from: 'noreply@airmedfoundation.com'
 };
