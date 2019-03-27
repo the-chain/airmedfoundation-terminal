ed84101100b2ba157aed4b71839aecd905bd67b3
alexander <Alexander_yammine@hotmail.com>
2019-03-27 12:39:59 -0400

    Verify email controller

--- api/controllers/services/secure-rec/user/verify-email.js
+++ api/controllers/services/secure-rec/user/verify-email.js
@@ -8,16 +8,43 @@ module.exports = {
 
 
   inputs: {
-
+    hash: {
+      type: 'string'
+    }
   },
 
 
   exits: {
-
+    invalid: {
+      responseType: 'bad-combo',
+      description: 'Error data provided'
+    },
+    internalError: {
+      responseType: 'internal-error',
+      description: 'Error changing password'
+    }
   },
 
 
   fn: async function (inputs, exits) {
+
+    if ( inputs.hash === undefined )
+      return exits.invalid();
+
+    // Find user
+    var user = await User.findOne({
+      emailProofToken: inputs.hash
+    });
+    if ( !user )
+      return exits.invalid();
+    
+    // Update user
+    var updatedUser = await User.updateOne({ emailAddress: user.emailAddress })
+    .set({
+        emailVerification: 'active'
+    });
+    if (!updatedUser)
+      return exits.internalError();
 
     return exits.success();
 
