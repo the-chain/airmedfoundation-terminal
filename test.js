143274000da231d00917e140ff227c7f48fb2d92
alexander <Alexander_yammine@hotmail.com>
2019-04-28 18:54:26 -0400

    Crypto library test

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 var ipfs = require('./test/ipfs-test/ipfs-test');
 var fabric = require('./test/fabric-test/fabric-test');
+var crypto = require('./test/crypto/key-test');
 // IPFS TEST
 describe('InterPlanetary Filesystem API TEST', function() {
     ipfs.testDownloadAsync();
@@ -12,4 +13,15 @@ describe('Hyperledger Fabric API TEST', function(){
     this.timeout(60000);
     fabric.invoqueTransaction();
     fabric.queryChaincode();
+});
+// Crypto key test (Ursa Library)
+describe('Crypto Library test', function(){
+    crypto.generateKeyTest();
+    crypto.getPublicKeyTest();
+    crypto.isPublicKeyTest();
+    crypto.isPrivateKeyTest();
+    crypto.encryptHashTest();
+    crypto.decryptHashTest();
+    crypto.assertPublicKeyTest();
+    crypto.assertPrivateKeyTest();
 });
