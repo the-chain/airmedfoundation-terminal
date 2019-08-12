8905404ab1c1ab7f6282f057d72f5e781a1e445f
alexander <Alexander_yammine@hotmail.com>
2019-08-12 15:37:48 -0400

    Show prescription (incomplete)

--- api/controllers/services/secure-rec/prescription/show.js
+++ api/controllers/services/secure-rec/prescription/show.js
@@ -1,3 +1,5 @@
+const fabric = require('../../../../../fabric-api/chaincodeTransactions');
+const ursa = require('../../../../../crypto/keys');
 module.exports = {
 
     friendlyName: 'Secure Rec prescriptions',
@@ -8,19 +10,62 @@ module.exports = {
       success: {
         responseType: 'view',
         viewTemplatePath: 'services/secure-rec/prescription/index'
+      },
+      conflict: {
+        responseType: 'conflict',
+        description: 'Los parámetros proporcionados no son únicos.'
+      },
+      upload: {
+          responseType: 'error-upload',
+          description: 'Error uploading the image'
+      },
+      ipfs: {
+          responseType: 'ipfs-error2',
+          description: 'Error uploading the image'
+      },
+      ursa: {
+          responseType: 'ursa-error',
+          description: 'Error en la clave enviada'
+      },
+      fabric: {
+          responseType: 'fabric-error',
+          description: 'Error Hyperledger Fabric' 
+      },
+      internalError: {
+          responseType: 'internal-error',
+          description: 'Error changing password'
       }
     },
     
     fn: async function (inputs, exits) {
         let user = this.req.session.auth;
+        let result, pubKey = await ursa.getPublicKey(this.req.session.auth.privateKey);
+        try{
+          result = await fabric.queryChaincode('mychannel','Org1MSP','secureRec', 'queryPrescriptions', ['Papasito']);
+        }catch(err){
+          return exits.fabric();
+        }
+        result = JSON.parse(result[0].toString());
+        let prescriptions = new Array(), unusedPrescriptions = new Array();
+        if ( result.prescriptions !== undefined ) {
+          for (let i = 0; i < result.prescriptions.length; i++ ) {
+            if ( result.prescriptions[i].status === 'SPENT' ) {
+              prescriptions.push(result.prescriptions[i])
+            } else if ( result.prescriptions[i].status === 'UNSPENT' ){
+              unusedPrescriptions.push(result.prescriptions[i]);
+            } else if ( result.prescriptions[i].status === 'DELETED' ){
+              // Veremos
+            }
+          }
+        }
         if (user.type == 'patient'){
             let insuranceCompanies, pharmacies;
             insuranceCompanies = await Patient.findOne({ emailAddress: user.emailAddress }).populate('insurances');
             insuranceCompanies = insuranceCompanies.insurances;
             pharmacies = await Patient.findOne({ emailAddress: user.emailAddress }).populate('providers', { where: { type: 'pharmacy'}});
             pharmacies = pharmacies.providers;
-            return exits.success({ 'insuranceCompanies': insuranceCompanies, 'pharmacies': pharmacies });
+            return exits.success({ 'insuranceCompanies': insuranceCompanies, 'pharmacies': pharmacies, 'unusedPrescriptions': unusedPrescriptions, 'prescriptions': prescriptions });
         }
-        return exits.success();
+        return exits.success({'unusedPrescriptions': unusedPrescriptions, 'prescriptions': prescriptions});
     }    
 };
